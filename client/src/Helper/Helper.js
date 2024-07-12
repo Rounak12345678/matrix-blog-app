4e818d2806a5f442c119974cd6f6d2f86353ecb6
Rounak12345678 <98976226+Rounak12345678@users.noreply.github.com>
2024-07-12 16:34:47 +0530

    Update Helper.js

--- client/src/Helper/Helper.js
+++ client/src/Helper/Helper.js
@@ -3,7 +3,7 @@ import { parseCookies } from "nookies";
 
 
 
-const baseURL = "http://localhost:3000/api/v1";
+const baseURL = "http://localhost:5000/api/v1";
 
 export const axiosInstance = axios.create({
   baseURL,
