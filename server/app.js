1d62d53169f4fae8058595786b7163b5991f6b35
Rounak12345678 <98976226+Rounak12345678@users.noreply.github.com>
2024-07-12 17:28:37 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -4,6 +4,12 @@ const blogRouter = require("./routes/blogRoute");
 const commentRouter = require("./routes/commentRoute");
 const app = express();
 
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']  // Add 'Authorization' to allowed headers
+}));
+
 app.use(express.json());
 
 app.use("/api/v1/users", userRouter);
